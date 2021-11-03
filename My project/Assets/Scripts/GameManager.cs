8c6a89f61ffdc3f531a5971e3f5a5c60f8df1306
guidaohuangxing <guidao12@163.com>
2021-11-03 15:00:44 -0400

    level1

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ public class GameManager : MonoBehaviour
     public GameObject latestGazeObject;
     public PlayerMovement player;
     public MouseLook playerEye;
+    public bool moveTwoPlayer = false;
+    public Vector3 TargetPos;
+    public GameObject AI;
+    public float distanceOffset = 10;
     private void Start()
     {
         player = FindObjectOfType<PlayerMovement>();
@@ -24,7 +28,25 @@ public class GameManager : MonoBehaviour
             if (latestGazeObject.GetComponent<FakeAI>().LookAtPlayer())
             {
                 player.canMove = false;
+                playerEye.limitRotate = true;
+                AI = latestGazeObject;
+                TargetPos = (AI.transform.position + player.transform.position) / 2;
+                moveTwoPlayer = true;
                 //print("gaze");
+            }
+        }
+        if (moveTwoPlayer)
+        {
+            if (Vector3.Distance(player.transform.position, AI.transform.position) > distanceOffset)
+            {
+                AI.transform.position = Vector3.Lerp(AI.transform.position, TargetPos, 0.002f);
+                player.transform.position = Vector3.Lerp(player.transform.position, TargetPos, 0.002f);
+            }
+            
+            print(Vector3.Distance(player.transform.position, AI.transform.position));
+            if (Vector3.Distance(player.transform.position, AI.transform.position) < distanceOffset)
+            {
+                moveTwoPlayer = false;
             }
         }
     }
--- My project/Assets/Scripts/MouseLook.cs
+++ My project/Assets/Scripts/MouseLook.cs
@@ -10,8 +10,9 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
 
     float xRotation = 0f;
+    float yRotation = 0f;
     public Canvas canvas;//use to get the screen width and length
-
+    public bool limitRotate = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,7 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (true)//GetComponentInParent<PlayerMovement>().canMove)
+        if (!limitRotate)//GetComponentInParent<PlayerMovement>().canMove)
         {
             GazePoint gazePoint = TobiiAPI.GetGazePoint();
             if (gazePoint.IsRecent())
@@ -36,7 +37,25 @@ public class MouseLook : MonoBehaviour
 
             }
         }
-        //GetFocusedObject();
+        else
+        {
+            GazePoint gazePoint = TobiiAPI.GetGazePoint();
+            if (gazePoint.IsRecent())
+            {
+                float mouseX = (gazePoint.Viewport.x - 0.5f) * 2 * mouseSensitivity;
+                float mouseY = (gazePoint.Viewport.y - 0.5f) * 2 * mouseSensitivity;
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -10f, 10f);
+                //transform.eulerAngles = new Vector3(ClampAngle(xRotation, -30f, 30f), 0, 0);
+                //print(xRotation);
+                yRotation += mouseX;
+                yRotation = Mathf.Clamp(yRotation, -10f, 10f);
+                transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+                // transform.localRotation = Quaternion.Euler(0, , 0f);
+                //playerBody.Rotate(Vector3.up * mouseX);
+
+            }
+        }
 
 
     }
@@ -63,5 +82,19 @@ public class MouseLook : MonoBehaviour
            
         }
         return null;
+    }
+
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < 90 || angle > 270)
+        {       // if angle in the critic region...
+            if (angle > 180) angle -= 360;  // convert all angles to -180..+180
+            if (max > 180) max -= 360;
+            if (min > 180) min -= 360;
+        }
+        angle = Mathf.Clamp(angle, min, max);
+        if (angle < 0) angle += 360;  // if angle negative, convert to 0..360
+        return angle;
     }
 }
